639d26cb4abf88ec77e1e3f4c147b08f3fc6d157
abuhasanmahmud <mahmud.me420@gmail.com>
2023-12-01 23:19:42 +0600

    s

--- src/app/all-product/page.jsx
+++ src/app/all-product/page.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 const page = () => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
-  // console.log("dataa", data);
+  console.log("dataa", data);
 
   useEffect(() => {
     const fetchData = async () => {
